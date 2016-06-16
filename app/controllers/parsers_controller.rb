7ec676638a7d1f053bb662ef701dc2858868205c
Honelyn Go <hgo@trusted-name.com>
2016-06-16 22:36:07 +0800

    moving view to partial

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -6,5 +6,6 @@ class ParsersController < ApplicationController
     file = params[:parsers][:file]
     file_data = file.respond_to?(:read) ? file.read : nil
     @data_hash = file_data ? JSON.parse(file_data) : {}
+    render action: :index
   end
 end
